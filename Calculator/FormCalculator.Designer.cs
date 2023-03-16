1f987cc2289ee247cad62735fb4d10c5ed33fa35
Daniil Aristov <danilka-aristov@mail.ru>
2023-03-16 14:34:35 +0300

    Добавьте файлы проекта.

---
+++ Calculator/FormCalculator.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace Calculator
+{
+    partial class FormCalculator
+    {
+        private System.ComponentModel.IContainer components = null;
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCalculator));
+            button_1 = new Button();
+            button_2 = new Button();
+            button_3 = new Button();
+            button_4 = new Button();
+            button_5 = new Button();
+            button_6 = new Button();
+            button_7 = new Button();
+            button_8 = new Button();
+            button_9 = new Button();
+            button_sign = new Button();
+            button_0 = new Button();
+            button_point = new Button();
+            button_result = new Button();
+            button_plus = new Button();
+            button_subtraction = new Button();
+            button_multiply = new Button();
+            button_division = new Button();
+            button_delete = new Button();
+            button_C = new Button();
+            inputTextBox = new TextBox();
+            outputTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // button_1
+            // 
+            button_1.BackColor = Color.FromArgb(64, 64, 64);
+            button_1.FlatAppearance.BorderSize = 0;
+            button_1.FlatStyle = FlatStyle.Flat;
+            button_1.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_1.ForeColor = SystemColors.ButtonFace;
+            button_1.Location = new Point(11, 303);
+            button_1.Name = "button_1";
+            button_1.Size = new Size(78, 46);
+            button_1.TabIndex = 0;
+            button_1.Text = "1";
+            button_1.UseVisualStyleBackColor = false;
+            button_1.Click += button_1_Click;
+            // 
+            // button_2
+            // 
+            button_2.BackColor = Color.FromArgb(64, 64, 64);
+            button_2.FlatAppearance.BorderSize = 0;
+            button_2.FlatStyle = FlatStyle.Flat;
+            button_2.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_2.ForeColor = SystemColors.ButtonFace;
+            button_2.Location = new Point(95, 303);
+            button_2.Name = "button_2";
+            button_2.Size = new Size(78, 46);
+            button_2.TabIndex = 1;
+            button_2.Text = "2";
+            button_2.UseVisualStyleBackColor = false;
+            button_2.Click += button_2_Click;
+            // 
+            // button_3
+            // 
+            button_3.BackColor = Color.FromArgb(64, 64, 64);
+            button_3.FlatAppearance.BorderSize = 0;
+            button_3.FlatStyle = FlatStyle.Flat;
+            button_3.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_3.ForeColor = SystemColors.ButtonFace;
+            button_3.Location = new Point(179, 303);
+            button_3.Name = "button_3";
+            button_3.Size = new Size(78, 46);
+            button_3.TabIndex = 2;
+            button_3.Text = "3";
+            button_3.UseVisualStyleBackColor = false;
+            button_3.Click += button_3_Click;
+            // 
+            // button_4
+            // 
+            button_4.BackColor = Color.FromArgb(64, 64, 64);
+            button_4.FlatAppearance.BorderSize = 0;
+            button_4.FlatStyle = FlatStyle.Flat;
+            button_4.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_4.ForeColor = SystemColors.ButtonFace;
+            button_4.Location = new Point(11, 250);
+            button_4.Name = "button_4";
+            button_4.Size = new Size(78, 46);
+            button_4.TabIndex = 3;
+            button_4.Text = "4";
+            button_4.UseVisualStyleBackColor = false;
+            button_4.Click += button_4_Click;
+            // 
+            // button_5
+            // 
+            button_5.BackColor = Color.FromArgb(64, 64, 64);
+            button_5.FlatAppearance.BorderSize = 0;
+            button_5.FlatStyle = FlatStyle.Flat;
+            button_5.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_5.ForeColor = SystemColors.ButtonFace;
+            button_5.Location = new Point(95, 250);
+            button_5.Name = "button_5";
+            button_5.Size = new Size(78, 46);
+            button_5.TabIndex = 4;
+            button_5.Text = "5";
+            button_5.UseVisualStyleBackColor = false;
+            button_5.Click += button_5_Click;
+            // 
+            // button_6
+            // 
+            button_6.BackColor = Color.FromArgb(64, 64, 64);
+            button_6.FlatAppearance.BorderSize = 0;
+            button_6.FlatStyle = FlatStyle.Flat;
+            button_6.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_6.ForeColor = SystemColors.ButtonFace;
+            button_6.Location = new Point(179, 250);
+            button_6.Name = "button_6";
+            button_6.Size = new Size(78, 46);
+            button_6.TabIndex = 5;
+            button_6.Text = "6";
+            button_6.UseVisualStyleBackColor = false;
+            button_6.Click += button_6_Click;
+            // 
+            // button_7
+            // 
+            button_7.BackColor = Color.FromArgb(64, 64, 64);
+            button_7.FlatAppearance.BorderSize = 0;
+            button_7.FlatStyle = FlatStyle.Flat;
+            button_7.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_7.ForeColor = SystemColors.ButtonFace;
+            button_7.Location = new Point(11, 198);
+            button_7.Name = "button_7";
+            button_7.Size = new Size(78, 46);
+            button_7.TabIndex = 6;
+            button_7.Text = "7";
+            button_7.UseVisualStyleBackColor = false;
+            button_7.Click += button_7_Click;
+            // 
+            // button_8
+            // 
+            button_8.BackColor = Color.FromArgb(64, 64, 64);
+            button_8.FlatAppearance.BorderSize = 0;
+            button_8.FlatStyle = FlatStyle.Flat;
+            button_8.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_8.ForeColor = SystemColors.ButtonFace;
+            button_8.Location = new Point(95, 198);
+            button_8.Name = "button_8";
+            button_8.Size = new Size(78, 46);
+            button_8.TabIndex = 7;
+            button_8.Text = "8";
+            button_8.UseVisualStyleBackColor = false;
+            button_8.Click += button_8_Click;
+            // 
+            // button_9
+            // 
+            button_9.BackColor = Color.FromArgb(64, 64, 64);
+            button_9.FlatAppearance.BorderSize = 0;
+            button_9.FlatStyle = FlatStyle.Flat;
+            button_9.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_9.ForeColor = SystemColors.ButtonFace;
+            button_9.Location = new Point(179, 198);
+            button_9.Name = "button_9";
+            button_9.Size = new Size(78, 46);
+            button_9.TabIndex = 8;
+            button_9.Text = "9";
+            button_9.UseVisualStyleBackColor = false;
+            button_9.Click += button_9_Click;
+            // 
+            // button_sign
+            // 
+            button_sign.BackColor = Color.FromArgb(50, 50, 50);
+            button_sign.FlatAppearance.BorderSize = 0;
+            button_sign.FlatStyle = FlatStyle.Flat;
+            button_sign.Font = new Font("Arial", 17.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button_sign.ForeColor = SystemColors.ButtonFace;
+            button_sign.Location = new Point(179, 146);
+            button_sign.Name = "button_sign";
+            button_sign.Size = new Size(78, 46);
+            button_sign.TabIndex = 9;
+            button_sign.Text = "+/-";
+            button_sign.UseVisualStyleBackColor = false;
+            button_sign.Click += button_sign_Click;
+            // 
+            // button_0
+            // 
+            button_0.BackColor = Color.FromArgb(64, 64, 64);
+            button_0.FlatAppearance.BorderSize = 0;
+            button_0.FlatStyle = FlatStyle.Flat;
+            button_0.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_0.ForeColor = SystemColors.ButtonFace;
+            button_0.Location = new Point(11, 354);
+            button_0.Name = "button_0";
+            button_0.Size = new Size(78, 46);
+            button_0.TabIndex = 10;
+            button_0.Text = "0";
+            button_0.UseVisualStyleBackColor = false;
+            button_0.Click += button_0_Click;
+            // 
+            // button_point
+            // 
+            button_point.BackColor = Color.FromArgb(64, 64, 64);
+            button_point.FlatAppearance.BorderSize = 0;
+            button_point.FlatStyle = FlatStyle.Flat;
+            button_point.Font = new Font("Arial", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_point.ForeColor = SystemColors.ButtonFace;
+            button_point.Location = new Point(95, 354);
+            button_point.Name = "button_point";
+            button_point.Size = new Size(78, 46);
+            button_point.TabIndex = 11;
+            button_point.Text = ",";
+            button_point.TextAlign = ContentAlignment.TopCenter;
+            button_point.UseVisualStyleBackColor = false;
+            button_point.Click += button_point_Click;
+            // 
+            // button_result
+            // 
+            button_result.BackColor = Color.FromArgb(156, 134, 217);
+            button_result.FlatAppearance.BorderSize = 0;
+            button_result.FlatStyle = FlatStyle.Flat;
+            button_result.Font = new Font("Arial", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button_result.ForeColor = SystemColors.ButtonFace;
+            button_result.Location = new Point(179, 354);
+            button_result.Name = "button_result";
+            button_result.Size = new Size(162, 46);
+            button_result.TabIndex = 12;
+            button_result.Text = "=";
+            button_result.UseVisualStyleBackColor = false;
+            button_result.Click += button_result_Click;
+            // 
+            // button_plus
+            // 
+            button_plus.BackColor = Color.FromArgb(50, 50, 50);
+            button_plus.FlatAppearance.BorderSize = 0;
+            button_plus.FlatStyle = FlatStyle.Flat;
+            button_plus.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button_plus.ForeColor = SystemColors.ButtonFace;
+            button_plus.Location = new Point(263, 302);
+            button_plus.Name = "button_plus";
+            button_plus.Size = new Size(78, 46);
+            button_plus.TabIndex = 13;
+            button_plus.Text = "+";
+            button_plus.UseVisualStyleBackColor = false;
+            button_plus.Click += button_plus_Click;
+            // 
+            // button_subtraction
+            // 
+            button_subtraction.BackColor = Color.FromArgb(50, 50, 50);
+            button_subtraction.FlatAppearance.BorderSize = 0;
+            button_subtraction.FlatStyle = FlatStyle.Flat;
+            button_subtraction.Font = new Font("Arial", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button_subtraction.ForeColor = SystemColors.ButtonFace;
+            button_subtraction.Location = new Point(263, 250);
+            button_subtraction.Name = "button_subtraction";
+            button_subtraction.Size = new Size(78, 46);
+            button_subtraction.TabIndex = 14;
+            button_subtraction.Text = "−";
+            button_subtraction.UseVisualStyleBackColor = false;
+            button_subtraction.Click += button_subtraction_Click;
+            // 
+            // button_multiply
+            // 
+            button_multiply.BackColor = Color.FromArgb(50, 50, 50);
+            button_multiply.FlatAppearance.BorderSize = 0;
+            button_multiply.FlatStyle = FlatStyle.Flat;
+            button_multiply.Font = new Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            button_multiply.ForeColor = SystemColors.ButtonFace;
+            button_multiply.Location = new Point(263, 198);
+            button_multiply.Name = "button_multiply";
+            button_multiply.Size = new Size(78, 46);
+            button_multiply.TabIndex = 15;
+            button_multiply.Text = "x";
+            button_multiply.UseVisualStyleBackColor = false;
+            button_multiply.Click += button_multiply_Click;
+            // 
+            // button_division
+            // 
+            button_division.BackColor = Color.FromArgb(50, 50, 50);
+            button_division.FlatAppearance.BorderSize = 0;
+            button_division.FlatStyle = FlatStyle.Flat;
+            button_division.Font = new Font("Arial", 21.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button_division.ForeColor = SystemColors.ButtonFace;
+            button_division.Location = new Point(263, 146);
+            button_division.Name = "button_division";
+            button_division.Size = new Size(78, 46);
+            button_division.TabIndex = 16;
+            button_division.Text = "÷";
+            button_division.UseVisualStyleBackColor = false;
+            button_division.Click += button_division_Click;
+            // 
+            // button_delete
+            // 
+            button_delete.BackColor = Color.FromArgb(50, 50, 50);
+            button_delete.FlatAppearance.BorderSize = 0;
+            button_delete.FlatStyle = FlatStyle.Flat;
+            button_delete.Font = new Font("Arial", 17.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button_delete.ForeColor = SystemColors.ButtonFace;
+            button_delete.Location = new Point(95, 146);
+            button_delete.Name = "button_delete";
+            button_delete.Size = new Size(78, 46);
+            button_delete.TabIndex = 17;
+            button_delete.Text = "⌫";
+            button_delete.UseVisualStyleBackColor = false;
+            button_delete.Click += button_delete_Click;
+            // 
+            // button_C
+            // 
+            button_C.BackColor = Color.FromArgb(50, 50, 50);
+            button_C.FlatAppearance.BorderSize = 0;
+            button_C.FlatStyle = FlatStyle.Flat;
+            button_C.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_C.ForeColor = SystemColors.ButtonFace;
+            button_C.Location = new Point(11, 146);
+            button_C.Name = "button_C";
+            button_C.Size = new Size(78, 46);
+            button_C.TabIndex = 18;
+            button_C.Text = "C";
+            button_C.UseVisualStyleBackColor = false;
+            button_C.Click += button_C_Click;
+            // 
+            // inputTextBox
+            // 
+            inputTextBox.Anchor = AnchorStyles.Top;
+            inputTextBox.BackColor = Color.FromArgb(22, 22, 22);
+            inputTextBox.BorderStyle = BorderStyle.None;
+            inputTextBox.Font = new Font("Arial", 39.75F, FontStyle.Bold, GraphicsUnit.Point);
+            inputTextBox.ForeColor = SystemColors.ButtonFace;
+            inputTextBox.Location = new Point(12, 67);
+            inputTextBox.Name = "inputTextBox";
+            inputTextBox.PlaceholderText = "0";
+            inputTextBox.ReadOnly = true;
+            inputTextBox.Size = new Size(329, 61);
+            inputTextBox.TabIndex = 19;
+            inputTextBox.TextAlign = HorizontalAlignment.Right;
+            // 
+            // outputTextBox
+            // 
+            outputTextBox.Anchor = AnchorStyles.Top;
+            outputTextBox.BackColor = Color.FromArgb(22, 22, 22);
+            outputTextBox.BorderStyle = BorderStyle.None;
+            outputTextBox.Font = new Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            outputTextBox.ForeColor = SystemColors.ButtonFace;
+            outputTextBox.Location = new Point(11, 21);
+            outputTextBox.Name = "outputTextBox";
+            outputTextBox.ReadOnly = true;
+            outputTextBox.Size = new Size(329, 28);
+            outputTextBox.TabIndex = 20;
+            outputTextBox.TextAlign = HorizontalAlignment.Right;
+            // 
+            // FormCalculator
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(22, 22, 22);
+            ClientSize = new Size(352, 412);
+            Controls.Add(outputTextBox);
+            Controls.Add(inputTextBox);
+            Controls.Add(button_C);
+            Controls.Add(button_delete);
+            Controls.Add(button_division);
+            Controls.Add(button_multiply);
+            Controls.Add(button_subtraction);
+            Controls.Add(button_plus);
+            Controls.Add(button_result);
+            Controls.Add(button_point);
+            Controls.Add(button_0);
+            Controls.Add(button_sign);
+            Controls.Add(button_9);
+            Controls.Add(button_8);
+            Controls.Add(button_7);
+            Controls.Add(button_6);
+            Controls.Add(button_5);
+            Controls.Add(button_4);
+            Controls.Add(button_3);
+            Controls.Add(button_2);
+            Controls.Add(button_1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "FormCalculator";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Калькулятор";
+            Load += FormCalculator_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button_1;
+        private Button button_2;
+        private Button button_3;
+        private Button button_4;
+        private Button button_5;
+        private Button button_6;
+        private Button button_7;
+        private Button button_8;
+        private Button button_9;
+        private Button button_sign;
+        private Button button_0;
+        private Button button_point;
+        private Button button_result;
+        private Button button_plus;
+        private Button button_subtraction;
+        private Button button_multiply;
+        private Button button_division;
+        private Button button_delete;
+        private Button button_C;
+        private TextBox inputTextBox;
+        private TextBox outputTextBox;
+    }
+}
---
+++ Calculator/FormCalculator.cs
@@ -0,0 +1,240 @@
+﻿namespace Calculator
+{
+    public partial class FormCalculator : Form
+    {
+        public FormCalculator()
+        {
+            InitializeComponent();
+        }
+
+        string command = "";
+        double num1, num2;
+
+        private void FormCalculator_Load(object sender, EventArgs e)
+        {
+            outputTextBox.Clear();
+            inputTextBox.Clear();
+        }
+
+        private void button_1_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "1"; }
+        }
+
+        private void button_2_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "2"; }
+        }
+
+        private void button_3_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "3"; }
+        }
+
+        private void button_4_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "4"; }
+        }
+
+        private void button_5_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "5"; }
+        }
+
+        private void button_6_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "6"; }
+        }
+
+        private void button_7_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "7"; }
+        }
+
+        private void button_8_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "8"; }
+        }
+
+        private void button_9_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11) { inputTextBox.Text += "9"; }
+        }
+
+        private void button_0_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length < 11)
+            {
+                inputTextBox.Text += "0";
+            }
+        }
+
+        private void button_delete_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0)
+            {
+                inputTextBox.Text = inputTextBox.Text.Remove(inputTextBox.Text.Length - 1);
+            }
+        }
+
+        private void button_point_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0 && !inputTextBox.Text.Contains(','))
+            {
+                inputTextBox.Text += ",";
+            }
+        }
+
+        private void button_sign_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0)
+            {
+                inputTextBox.Text = Convert.ToString(Convert.ToDouble(inputTextBox.Text) * (-1));
+            }
+        }
+
+        private void button_C_Click(object sender, EventArgs e)
+        {
+            inputTextBox.Clear();
+            outputTextBox.Clear();
+            command = "";
+            num1 = 0;
+            num2 = 0;
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0 && outputTextBox.Text.Length == 0)
+            {
+                try
+                {
+                    num1 = Convert.ToDouble(inputTextBox.Text);
+                    command = "+";
+                    outputTextBox.Text = num1.ToString() + " + ";
+                    inputTextBox.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Неккоректное число!");
+                    inputTextBox.Clear();
+                }
+            }
+        }
+
+        private void button_subtraction_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0 && outputTextBox.Text.Length == 0)
+            {
+                try
+                {
+                    num1 = Convert.ToDouble(inputTextBox.Text);
+                    command = "-";
+                    outputTextBox.Text = num1.ToString() + " - ";
+                    inputTextBox.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Неккоректное число!");
+                    inputTextBox.Clear();
+                }
+            }
+        }
+
+        private void button_multiply_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0 && outputTextBox.Text.Length == 0)
+            {
+                try
+                {
+                    num1 = Convert.ToDouble(inputTextBox.Text);
+                    command = "x";
+                    outputTextBox.Text = num1.ToString() + " x ";
+                    inputTextBox.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Неккоректное число!");
+                    inputTextBox.Clear();
+                }
+            }
+        }
+
+        private void button_division_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0 && outputTextBox.Text.Length == 0)
+            {
+                try
+                {
+                    num1 = Convert.ToDouble(inputTextBox.Text);
+                    command = "÷";
+                    outputTextBox.Text = num1.ToString() + " ÷ ";
+                    inputTextBox.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Неккоректное число!");
+                    inputTextBox.Clear();
+                }
+            }
+        }
+
+        private void button_result_Click(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0 && outputTextBox.Text.Length > 0)
+            {
+                try
+                {
+                    num2 = Convert.ToDouble(inputTextBox.Text);
+                    double result = calculation(num1, num2, command);
+                    if (errorZero != 1)
+                    {
+                        inputTextBox.Text = result.ToString();
+                    }
+                    else
+                    {
+                        outputTextBox.Text = "Деление на ноль!";
+                        inputTextBox.Clear();
+                        errorZero = 0;
+                        return;
+                    }
+                    outputTextBox.Clear();
+                }
+                catch
+                {
+                    MessageBox.Show("Неккоректное число!");
+                    inputTextBox.Clear();
+                }
+            }
+        }
+
+        int errorZero = 0;
+        private double calculation(double num1, double num2, string command)
+        {
+            double result = 0;
+            switch (command)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "x":
+                    result = num1 * num2;
+                    break;
+                case "÷":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        result = 0;
+                        errorZero = 1;
+                    }
+                    break;
+            }
+            return result;
+        }
+    }
+}
